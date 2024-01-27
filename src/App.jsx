058edcb77b4c5bba635374d75ce6df120776badf
Nasiful Islam Nasif <nasifulislamnasif23@gmail.com>
2024-01-28 01:55:39 +0600

    practice accordian

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Marquee from "react-fast-marquee";
 import nike from "../src/assets/marqee-image/Nike-Logo.png"
 import microsoft from "../src/assets/marqee-image/microsoft.jpg"
 import tommy from "../src/assets/marqee-image/Tommy-Hilfiger-logo.png"
-
+import Faq from './components/faq';
+import { Button } from "@material-tailwind/react";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
     <>
     
       <div className='max-w-[1200px] mx-auto p-12'>
-      <h1 className='font-bold text-3xl pb-5'>React Marquee test</h1>
+      <h1 className='font-bold text-3xl pb-8'>React Marquee test</h1>
         <Marquee gradient="true" speed={30}>
           <div className='w-40 px-3'>
             <img src={nike} alt="Nike" />
@@ -40,45 +41,12 @@ function App() {
           </div>
           <div className='w-40 px-3'>
             <img src={tommy} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={nike} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={microsoft} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={tommy} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={nike} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={microsoft} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={tommy} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={nike} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={microsoft} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={tommy} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={nike} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={microsoft} alt="Nike" />
-          </div>
-          <div className='w-40 px-3'>
-            <img src={tommy} alt="Nike" />
-          </div>
+          </div>    
         </Marquee>
       </div>
+      <Faq></Faq>
+
+      
 
     </>
   )
